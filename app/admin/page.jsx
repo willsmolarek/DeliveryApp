acd8c2bac1f545202659e7830385d840e87861b7
Will Smolarek <willcskfeed@gmail.com>
2025-10-20 13:50:23 -0300

    Add admin page

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -2,8 +2,7 @@ import OrdersManagement from '@/components/admin/OrdersManagement'
 
 export default function AdminPage() {
   return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6">Gerenciamento de Pedidos</h1>
+    <div className="min-h-screen bg-gray-50">
       <OrdersManagement />
     </div>
   )
